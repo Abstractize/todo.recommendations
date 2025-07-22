8a2a0e36e6a0408da6f2ca5aac7eacbc786accac
Gabriel Abarca Aguilar <gababarcaa@gmail.com>
2025-07-21 22:11:09 -0600

    Add JWT Auth (#1)

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,10 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     app_name: str = "Recommendation Service"
+    jwt_issuer: str = ""
+    jwt_audience: str = ""
+    jwt_key: str = ""
+    jwt_algorithm: str = "HS256"
 
     class Config:
         env_file = ".env"
